69fbff6ac5a5915ef4636a6eae142fec5725c80e
charmingruby <mrrubycanal3105@gmail.com>
2024-10-04 10:56:48 -0300

    feat: reference entity

--- src/domain/collectables/entities/reference.ts
+++ src/domain/collectables/entities/reference.ts
@@ -1,1 +1,53 @@
-export class Reference {}
+import { Entity } from '@/core/entities/entity'
+import { Identifier } from '@/core/entities/identifier'
+import { Optional } from '@/core/types/optional'
+
+export interface ReferenceProperties {
+  title: string
+  observation: string
+  url: string
+  priceInCents: number
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class Reference extends Entity<ReferenceProperties> {
+  get title(): string {
+    return this.props.title
+  }
+
+  get observation(): string {
+    return this.props.observation
+  }
+
+  get url(): string {
+    return this.props.url
+  }
+
+  get priceInCents(): number {
+    return this.props.priceInCents
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt
+  }
+
+  get updatedAt(): Date | null | undefined {
+    return this.props.updatedAt
+  }
+
+  static create(
+    props: Optional<ReferenceProperties, 'createdAt'>,
+    id?: Identifier,
+  ) {
+    const reference = new Reference(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return reference
+  }
+}
